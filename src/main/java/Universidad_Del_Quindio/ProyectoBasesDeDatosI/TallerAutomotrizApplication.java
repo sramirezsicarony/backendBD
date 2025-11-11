ebe2e0afccb91c174eaa4d7c2745435bb923898f
sramirezsicarony <sramirezsicarony@gmail.com>
2025-11-10 23:33:08 -0500

    Subiendo proyecto inicial

---
+++ src/main/java/Universidad_Del_Quindio/ProyectoBasesDeDatosI/TallerAutomotrizApplication.java
@@ -0,0 +1,13 @@
+package Universidad_Del_Quindio.ProyectoBasesDeDatosI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerAutomotrizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerAutomotrizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Universidad_Del_Quindio/ProyectoBasesDeDatosI/TallerAutomotrizApplicationTests.java
@@ -0,0 +1,13 @@
+package Universidad_Del_Quindio.ProyectoBasesDeDatosI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerAutomotrizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
